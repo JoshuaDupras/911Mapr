34741d87a8ec573a0d586f90f2bdf088b616c633
JoshuaDupras <hello@joshuadupras.com>
2022-04-12 01:24:39 -0400

    Revert "Add nightly backup of redis to S3"
    This reverts commit 83528ac542a4edbe53e9fa58784226227e682f37.

--- redis/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "running startup.sh"
-
-apt-get update
-apt-get --assume-yes install cron
-
-(crontab -l 2>/dev/null; echo "0 5 * * * /root/nightly-backup.sh") | crontab -
-
-echo "startup.sh complete"
